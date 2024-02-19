de5a5232dc11e980997c8ae8c456386bb2b5da58
Nitish Raj <nitishrajpandey0707@gmail.com>
2024-02-19 18:16:26 +0530

    initial setup

---
+++ src/Components/BannerSection.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function BannerSection() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1></h1>
+          <p></p>
+          <div>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BannerSection;
